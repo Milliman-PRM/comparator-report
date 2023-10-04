e433e44e3848f33f55c8e6c7caeec1e709406ebd
giang.vu <giang.vu@milliman.com>
2023-10-04 14:21:52 -0400

    define custom functions for grabbing hcc info

--- scripts/outputs/risk_scores.py
+++ scripts/outputs/risk_scores.py
@@ -7,14 +7,16 @@
 """
 import logging
 import os
-
-from datetime import date
-from prm.dates.utils import date_as_month
+import datetime
+import typing
 from prm.spark.app import SparkApp
 import pyspark.sql.functions as spark_funcs
+
 from pyspark.sql import Window
 import comparator_report.meta.project
 import prm.riskscr.hcc
+from prm.dates.windows import ClaimDateWindow
+import cms_hcc.pyspark_api
 
 LOGGER = logging.getLogger(__name__)
 META_SHARED = comparator_report.meta.project.gather_metadata()
@@ -115,32 +117,64 @@ HCC_COLS = [
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 # =============================================================================
 
+def _create_time_periods(
+    sparkapp: SparkApp, meta_shared: typing.Mapping[str, typing.Any]
+) -> cms_hcc.pyspark_api.TimePeriods:
+    """Create time periods input parameter for CMS-HCC processing"""
+    modeling_windows = sparkapp.load_df(PATH_INPUTS / "time_periods.parquet").where(
+        spark_funcs.col("time_period_id").isin(["202312", "202412"]) #not sure about this part
+    )
+
+    iter_time_windows = modeling_windows.collect()
+    time_periods = []
+    for time_window in iter_time_windows:
+        diag_date_window = ClaimDateWindow(
+            incstart=time_window.risk_score_date_start,
+            incend=time_window.risk_score_date_end,
+            paidthru=max(
+                meta_shared["date_latestpaid"], time_window.risk_score_date_end
+            ),
+        )
+        payment_date_window = ClaimDateWindow(
+            incstart=time_window.reporting_date_start,
+            incend=time_window.reporting_date_end,
+            paidthru=max(
+                meta_shared["date_latestpaid"], time_window.reporting_date_end
+            ),
+        )
+        process_params = cms_hcc.pyspark_api.ProcessParams(
+            diag_date_range=diag_date_window,
+            payment_date_range=payment_date_window,
+            version=prm.riskscr.hcc.HCC_VERSION,
+            esrd_version=prm.riskscr.hcc.ESRD_VERSION,
+            name_period=time_window.time_period_id,
+            date_as_of=payment_date_window.incstart + datetime.timedelta(days=31),
+        )
+        time_periods.append(process_params)
+    return time_periods
+
+
 def main() -> int:
     """Create a table with count of HCC markers for each member"""
     sparkapp = SparkApp(META_SHARED['pipeline_signature'])
     
-    df_outclaims = sparkapp.load_df(
-            META_SHARED[73, "out"] / "outclaims_prm.parquet"
-    )
-    
-    df_member = sparkapp.load_df(
-            META_SHARED[35, "out"] / "member.parquet"
-    )
-    
-    df_member_time = sparkapp.load_df(
-            META_SHARED[35, "out"] / "member_time.parquet"
-    )
-
     dfs_input = {
-        path.stem: sparkapp.load_df(path)
-        for path in [
-            PATH_INPUTS / 'member_time_windows.parquet',
-            PATH_INPUTS / 'time_periods.parquet',
-            PATH_INPUTS / 'members.parquet',
-            PATH_INPUTS / 'risk_scores.parquet',
-            PATH_INPUTS / 'outclaims.parquet',
-        ]
+        "outclaims": sparkapp.load_df(
+            META_SHARED[73, "out"] / "outclaims_prm.parquet"
+        ),
+        "member": sparkapp.load_df(
+            META_SHARED[35, "out"] / "member.parquet"
+        ),
+        "member_time": sparkapp.load_df(
+            META_SHARED[35, "out"] / "member_time.parquet"
+        ),
     }
+    
+    time_periods = _create_time_periods(
+        sparkapp, META_SHARED
+    )  ### Ensure same paid through date for all ACOs
+    
+    hcc_results = prm.riskscr.hcc.calc_hccs(sparkapp, dfs_input, time_periods) #join "member" with "feature_info" from this dictionary
     
     return 0
 
