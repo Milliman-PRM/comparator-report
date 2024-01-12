dabd71d8ac4bc068e3e32e1745784fed34307995
siyi.lu <siyi.lu@milliman.com>
2024-01-12 15:37:28 -0500

    update truncation

--- scripts/outputs/truncation.py
+++ scripts/outputs/truncation.py
@@ -161,16 +161,16 @@ def truncation_summary(
         'truncation_threshold',
         spark_funcs.when(
             spark_funcs.col('elig_status') == 'Aged Non-Dual',
-            138974 * spark_funcs.col('memmos') / 12,
+            147189  * spark_funcs.col('memmos') / 12,
         ).when(
             spark_funcs.col('elig_status') == 'Aged Dual',
-            225780 * spark_funcs.col('memmos') / 12,
+            236991  * spark_funcs.col('memmos') / 12,
         ).when(
             spark_funcs.col('elig_status') == 'Disabled',
-            167346 * spark_funcs.col('memmos') / 12,
+            177548 * spark_funcs.col('memmos') / 12,
         ).when(
             spark_funcs.col('elig_status') == 'ESRD',
-            483440 * spark_funcs.col('memmos') / 12,
+            507077  * spark_funcs.col('memmos') / 12,
         ).otherwise(
             99999999999
         )
